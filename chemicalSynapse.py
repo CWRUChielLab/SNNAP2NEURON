d910b75b95f3c3f7fbd97c091512f72e61645d33
madawa87 <abeywardhanajamm@gmail.com>
2019-05-25 09:44:41 -0400

    fix: file headers

--- chemicalSynapse.py
+++ chemicalSynapse.py
@@ -1,3 +1,21 @@
+# This file is part of SNNAP2NEURON.
+#
+# Copyright (C) 2019 Jayalath A M M Abeywardhana, Jeffrey Gill, Reid Bolding,
+# Peter Thomas
+#
+# SNNAP2NEURON is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# SNNAP2NEURON is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SNNAP2NEURON.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import re
 
--- treatment.py
+++ treatment.py
@@ -1,3 +1,19 @@
+# This file is part of SNNAP2NEURON.
+#
+# Copyright (C) 2019 Jayalath A M M Abeywardhana, Jeffrey Gill, Reid Bolding,
+# Peter Thomas
+#
+# SNNAP2NEURON is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# SNNAP2NEURON is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
 import sys
 import re
 import os
