9f9ea4f8d5ee62f1435cad6797ccd4af18b29f06
madawa87 <abeywardhanajamm@gmail.com>
2019-07-29 13:33:51 -0400

    added command line argument support

--- readS.py
+++ readS.py
@@ -19,18 +19,21 @@
 import sys
 import re
 import os
+import argparse
 
 from simulation import Simulation as sim
 from nrnModelPoint import NRNModelPoint
 from nrnModelDist import NRNModelDist
 
-def parse2Hoc(filename):
+def parse2Hoc(filename, cond):
     # create simulation object. 
     snnapSim = sim(filename)
 
     # write model in NEURON
-    #nrnModel = NRNModelPoint(snnapSim)
-    nrnModel = NRNModelDist(snnapSim)
+    if cond == 'p':
+        nrnModel = NRNModelPoint(snnapSim)
+    elif cond == 'd':
+        nrnModel = NRNModelDist(snnapSim)
     return snnapSim
 
 def printSim(sim):
@@ -38,17 +41,33 @@ def printSim(sim):
 
     
 if __name__ == "__main__":
-    print "main"
-    if len(sys.argv) > 1:
-        filePath = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="path to SNNAP .smu file")
+    parser.add_argument("-c", "--cond", choices=['p', 'd'], default='p',
+                        help="representation of conductances (point 'p', or distributed 'd')")
+    args = parser.parse_args()
+    filePath = args.input
+
+    cond = ''
+    if args.cond == 'p':
+        cond = 'p'
+        print "Conductances will be as point mechanisms"
+    elif args.cond == 'd':
+        cond = 'd'
+        print "Conductances will be as distributed mechanisms"
     else:
-        print("usage: parseSNNAP <simulationFile>")
-        filePath = "model/hhNetwork.smu"
+        print "choises for --cond are either 'p' or 'd'"
+    
+    # if len(sys.argv) > 1:
+    #     filePath = sys.argv[1]
+    # else:
+    #     print("usage: parseSNNAP <simulationFile>")
+    #     filePath = "model/hhNetwork.smu"
 
     splitedFilePath = filePath.split(os.sep)
 
     simFilePath = splitedFilePath[:-1]
     simFileName = splitedFilePath[len(splitedFilePath)-1]
     
-    currSim = parse2Hoc(filePath)
+    currSim = parse2Hoc(filePath, cond)
 
